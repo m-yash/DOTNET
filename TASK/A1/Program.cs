c886511107f0b269101f40880701ad9d0e1dcd34
Yash Mistry <61091300+m-yash@users.noreply.github.com>
2023-09-21 01:21:59 -0400

    update

--- TASK/A1/Program.cs
+++ TASK/A1/Program.cs
@@ -6,14 +6,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("STORE \n");
+        // Console.WriteLine("STORE \n");
 
         try{
             Console.WriteLine("Do you have a loyalty card? (yes/no)");
-            var loyalty = Console.ReadLine();
+            string loyalty = Console.ReadLine();
             
 
-            Console.WriteLine("Available Grocery Items: \nApple - $1.00 \nBanana - $0.50 \nMilk - $2.50 \nBread - $2.00 \nEggs - $1.75 ");
+            Console.WriteLine("\nAvailable Grocery Items: \nApple - $1.00 \nBanana - $0.50 \nMilk - $2.50 \nBread - $2.00 \nEggs - $1.75\n");
             Console.WriteLine("Enter the quantity of Apple (0 for none)");
             int apple = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the quantity of Banana (0 for none)");
@@ -27,26 +27,56 @@ class Program
 
 
             string receipt = "";
-            // if(apple > 0){
-            //     receipt += $"Apple x{apple}\n";
-            // }
-            // else 
-            // if(loyalty == "yes"){
+            if(apple > 0){
+                receipt += $"Apple x{apple}\n";
+            }
+            if(banana > 0){
+                receipt += $"Banana x{banana}\n";
+            }
+            if(milk > 0){
+                receipt += $"Milk x{milk}\n";
+            }
+            if(bread > 0){
+                receipt += $"Bread x{bread}\n";
+            }
+            if(eggs > 0){
+                receipt += $"Eggs x{eggs}\n";
+            }
 
-            // }
+
             
             Console.WriteLine("\nReceipt:");
-            Console.WriteLine($"Apple x{apple}\nApple x{apple}\nApple x{apple}\n");
+            Console.WriteLine(receipt);
 
-            float subTotal = (float)(1.50*apple) + (float)(0.50*banana) + (float)(2.50*milk) + (float)(2.50*bread) + (float)(1.75*eggs);
-            Console.WriteLine("Subtotal: $"+subTotal);
+            // float subTotal = (float)(1.50*apple) + (float)(0.50*banana) + (float)(2.50*milk) + (float)(2.50*bread) + (float)(1.75*eggs);
+            // float subTotal = 1.50f *apple + 0.50f *banana + 2.50f*milk + 2.50f*bread + 1.75f*eggs;
+            double subTotal = 1.50*apple + 0.50*banana + 2.50*milk + 2.50*bread + 1.75*eggs;
+            
+            if(loyalty.Contains("yes")){
+                Console.WriteLine("discount");
+                double tenPercentOfSubTotal = subTotal * 0.10;
 
-            float Taxof13 = subTotal * 0.13f;
-            Console.WriteLine("Tax (13%): $"+Taxof13);
+                double discountedSubTotal = subTotal - tenPercentOfSubTotal;
+                Console.WriteLine("Subtotal: $"+discountedSubTotal);
 
-            float = (float) subTotal + Taxof13;
-            Console.WriteLine("Total cost: $"+ );
+                double Taxof13 = discountedSubTotal * 0.13;
+                Console.WriteLine("Tax (13%): $"+Taxof13);
 
+                double totalAfterTax = subTotal + Taxof13;
+                Console.WriteLine("Total cost: $"+ totalAfterTax);
+            }
+            else if(loyalty.Contains("no")){
+                Console.WriteLine("Subtotal: $"+subTotal);
+
+                double Taxof13 = subTotal * 0.13;
+                Console.WriteLine("Tax (13%): $"+Taxof13);
+
+                double totalAfterTax = subTotal + Taxof13;
+                Console.WriteLine("Total cost: $"+ totalAfterTax);
+
+            }
+
+            
         }
         catch(Exception e){
             Console.WriteLine("Invalid input");
