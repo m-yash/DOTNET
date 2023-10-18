bfd0f18871226f439e60761407a7f58a4d1255c6
Yash Mistry <61091300+m-yash@users.noreply.github.com>
2023-10-18 17:01:20 -0400

    update

---
+++ A/A2NewTest/Program.cs
@@ -0,0 +1,381 @@
+﻿// See https://aka.m    s/new-console-template for more information
+namespace Assignment2;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        ReservationSystemSub r = new ReservationSystemSub();
+        r.StartSystem();
+
+        bool looper = true;
+
+        // List<Customer> customerInfo = new List<Customer>();
+
+
+        AllReserveration reserverationData = new AllReserveration();
+
+        // Hardcode data
+        reserverationData.AddReserverationMethod("VCT811", "Yash Mistry", "123-456-7890 ", 1, "Standard", "Child Car Seat ", 64.98);
+        reserverationData.AddReserverationMethod("HJS765", "John Kin", "547-875-6574", 1, "Economy", "Child Car Seat ", 44.98);
+        reserverationData.AddReserverationMethod("UYD976", "Sam Dam", "346-535-4765", 1, "Luxury", "Child Car Seat ", 94.97);
+        
+        List<string> service = new List<string>();
+        service.Add("GPS Navigation"); service.Add("Child Car Seat"); service.Add("Chauffeur Service"); service.Add("No Service");
+
+        while (looper)
+        {
+
+            //Setting up constant values for customer type
+            string[] CustomerTypeConst = { "", "Economy", "Standard", "Luxury" };
+
+            // Console.WriteLine(CustomerTypeConst[1]);
+
+            // for final total
+            double total = 0;
+
+            Console.WriteLine("Choose an option below:\n1.Create a reservation\n2.List all reservations\n3.Clear all reservations\n4.Exit the program");
+            var path = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            // when 1 entered
+            if (path == 1)
+            {
+                Console.WriteLine("Enter customer information:");
+
+                Console.WriteLine("Customer ID:");
+                string CustomerID = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine("Name:");
+                string CustomerName = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine("Phone Number:");
+                string CustomerPhoneNo = Console.ReadLine() ?? string.Empty;
+
+                Console.WriteLine("Customer Type:\n0.Regular\n1.Premium\n2.VIP");
+                int CustomerType = int.Parse(Console.ReadLine() ?? string.Empty);
+
+                // regular
+                if (CustomerType == 0)
+                {
+                    Console.WriteLine("Choose the number corresponding to the car type the customer wants:\n1.Economy Car Rental - $29.99/day\n2.Standard Car Rental - $49.99/day\n3.Luxury Car Rental - $79.99/day");
+                    int CustomerCarTypeRequirement = int.Parse(Console.ReadLine() ?? string.Empty);
+
+                    // economy
+                    if (CustomerCarTypeRequirement == 1)
+                    {
+                        total += 29.99;
+                    }
+                    // standard
+                    else if (CustomerCarTypeRequirement == 2)
+                    {
+                        total += 49.99;
+                    }
+                    // luxury
+                    else if (CustomerCarTypeRequirement == 3)
+                    {
+                        total += 79.99;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid car type selected");
+                    }
+
+
+                    Console.WriteLine("Does the customer want this additional service? (yes/no)\nGPS Navigation - $9.99/day");
+                    string CustomerAdditionalService = Console.ReadLine() ?? string.Empty;
+                    if (CustomerAdditionalService == "yes")
+                    {
+                        total += 9.99;
+                        reserverationData.AddReserverationMethod(CustomerID, CustomerName, CustomerPhoneNo, CustomerType, CustomerTypeConst[CustomerCarTypeRequirement], service[0], total);
+
+                        // showing total while successful reservation
+                        Console.WriteLine($"Your Total: ${total}");
+
+                        Console.WriteLine("Thank you! The reservation was successful.");
+                    }
+                    else
+                    {
+                        reserverationData.AddReserverationMethod(CustomerID, CustomerName, CustomerPhoneNo, CustomerType, CustomerTypeConst[CustomerCarTypeRequirement], service[3], total);
+                        Console.WriteLine($"Your Total: ${total}");
+
+                        Console.WriteLine("Thank you! The reservation was successful.");
+                        // Console.WriteLine(total);
+
+                    }
+                }
+                // Premium
+                else if (CustomerType == 1)
+                {
+                    Console.WriteLine("Choose the number corresponding to the car type the customer wants:\n1.Economy Car Rental - $29.99/day\n2.Standard Car Rental - $49.99/day\n3.Luxury Car Rental - $79.99/day");
+                    var CustomerCarTypeRequirement = int.Parse(Console.ReadLine() ?? string.Empty);
+                    if (CustomerCarTypeRequirement == 1)
+                    {
+                        total += 29.99;
+                    }
+                    else if (CustomerCarTypeRequirement == 2)
+                    {
+                        total += 49.99;
+                    }
+                    else if (CustomerCarTypeRequirement == 3)
+                    {
+                        total += 79.99;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid car type selected");
+                    }
+
+                    Console.WriteLine("Does the customer want this additional service? (yes/no)\nChild Car Seat - $14.99/day ");
+                    string CustomerAdditionalService = Console.ReadLine() ?? string.Empty;
+                    if (CustomerAdditionalService == "yes")
+                    {
+                        total += 14.99;
+                        reserverationData.AddReserverationMethod(CustomerID, CustomerName, CustomerPhoneNo, CustomerType, CustomerTypeConst[CustomerCarTypeRequirement], service[1], total);
+                        Console.WriteLine($"Your Total: ${total}");
+
+                        Console.WriteLine("Thank you! The reservation was successful.");
+                    }
+                    else
+                    {
+                        reserverationData.AddReserverationMethod(CustomerID, CustomerName, CustomerPhoneNo, CustomerType, CustomerTypeConst[CustomerCarTypeRequirement], service[3], total);
+                        Console.WriteLine($"Your Total: ${total}");
+                        Console.WriteLine("Thank you! The reservation was successful.");
+                    }
+                }
+                // VIP
+                else if (CustomerType == 2)
+                {
+                    Console.WriteLine("Choose the number corresponding to the car type the customer wants:\n1.Economy Car Rental - $29.99/day\n2.Standard Car Rental - $49.99/day\n3.Luxury Car Rental - $79.99/day");
+                    int CustomerCarTypeRequirement = int.Parse(Console.ReadLine() ?? string.Empty);
+                    if (CustomerCarTypeRequirement == 1)
+                    {
+                        total += 29.99;
+                    }
+                    else if (CustomerCarTypeRequirement == 2)
+                    {
+                        total += 49.99;
+                    }
+                    else if (CustomerCarTypeRequirement == 3)
+                    {
+                        total += 49.99;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid car type selected");
+                        break;
+                    }
+
+                    Console.WriteLine("Does the customer want this additional service? (yes/no)\nChauffeur Service - $99.99/day");
+                    var CustomerAdditionalService = Console.ReadLine();
+                    if (CustomerAdditionalService == "yes")
+                    {
+                        total += 99.99;
+                        reserverationData.AddReserverationMethod(CustomerID, CustomerName, CustomerPhoneNo, CustomerType, CustomerTypeConst[CustomerCarTypeRequirement], service[2], total);
+                        Console.WriteLine($"Your Total: ${total}");
+                        Console.WriteLine("Thank you! The reservation was successful.");
+                    }
+                    else
+                    {
+                        reserverationData.AddReserverationMethod(CustomerID, CustomerName, CustomerPhoneNo, CustomerType, CustomerTypeConst[CustomerCarTypeRequirement], service[3], total);
+                        Console.WriteLine($"Your Total: ${total}");
+                        Console.WriteLine("Thank you! The reservation was successful.");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid type selected");
+                }
+
+
+
+
+                // customerInfo.Add(new Customer(CustomerID, CustomerName, CustomerPhoneNo,CustomerType, CustomerCarTypeRequirement, CustomerAdditionalService));
+
+                // reserverationData.AddReserverationMethod(CustomerID, CustomerName, CustomerPhoneNo, CustomerType, CustomerCarTypeRequirement, CustomerAdditionalService);
+
+                // foreach (Customer customerdata in customerInfo)
+                // {
+                //     Console.WriteLine(customerdata.customerID +" "+ customerdata.customerName +" "+ customerdata.customerPhoneNo);
+                // }
+            }
+            else if (path == 2)
+            {
+                reserverationData.DisplayReserverationMethod();
+            }
+            else if (path == 3)
+            {
+                reserverationData.ClearReserverationMethod();
+            }
+            else if (path == 4)
+            {
+                looper = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+
+
+        }
+        // 1 input // user input
+        // customer.customerID = CustomerID;
+        // customer.customerName = CustomerName;
+        // customer.customerPhoneNo = CustomerPhoneNo;
+        // customer.customerType = CustomerType;
+        // customer.customerCarTypeRequirement = CustomerCarTypeRequirement;
+        // customer.customerAdditionalService = CustomerAdditionalService;
+
+        // reserverationData.AddReserverationMethod(customer);
+        // reserverationData.AddReserverationMethod(new Customer());
+
+
+        // reserverationData.DisplayReserverationMethod();
+
+        // When entered 2
+        // ClearReserveration clearRes = ClearReserveration();
+        // clearRes.ClearReserverationMethod();
+    }
+
+    // Inheritance (kept it simple)
+    class ReservationSystemSup
+    {
+        public void StartSystem()
+        {
+            Console.WriteLine("Reservation System\n");
+        }
+    }
+
+    class ReservationSystemSub : ReservationSystemSup
+    {
+
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class Customer : Person
+    {
+        public string customerID { get; set; }
+        // public string customerName { get; set; }
+        public string customerPhoneNo { get; set; }
+        public int customerType { get; set; }
+        public string customerCarTypeRequirement { get; set; }
+        public string customerAdditionalService { get; set; }
+        public double customerTotal { get; set; }
+        public Customer(string id, string name, string phone, int type, string req, string add, double tot) : base(name)
+        {
+            customerID = id;
+            // customerName = name;
+            customerPhoneNo = phone;
+            customerType = type;
+            customerCarTypeRequirement = req;
+            customerAdditionalService = add;
+            customerTotal = tot;
+
+        }
+    }
+
+
+    // Interface 
+    public interface IReservationable
+    {
+        void ClearReserverationMethod();
+        // void AddReserverationMethod(Customer customer);
+        void DisplayReserverationMethod();
+    }
+
+
+    // Implementing Interface
+    public class AllReserveration : IReservationable
+    {
+        public List<Customer> customerInfo = new List<Customer>();
+
+
+        // public void AddReserverationMethod(Customer customer){
+        //     customerInfo.Add(customer);
+        // }
+        public void AddReserverationMethod(string CustomerID, string name, string CustomerPhoneNo, int CustomerType, string CustomerCarTypeRequirement, string CustomerAdditionalService, double CustomerTotal)
+        {
+            customerInfo.Add(new Customer(CustomerID, name, CustomerPhoneNo, CustomerType, CustomerCarTypeRequirement, CustomerAdditionalService, CustomerTotal));
+        }
+
+        public void ClearReserverationMethod()
+        {
+            customerInfo.Clear();
+            Console.WriteLine("\nAll Reservations Cleared Successfully!");
+        }
+        public void DisplayReserverationMethod()
+        {
+            int cnt = 1;
+            foreach (Customer customerdata in customerInfo)
+            {
+
+                try
+                {
+                    Console.WriteLine($"\nReservation {cnt}: \nCustomer ID: XXX{customerdata.customerID.Substring(3)}\nName: {customerdata.Name}\nPhone Number: {customerdata.customerPhoneNo}\nCustomer Type: {customerdata.customerType}\nCar Type: {customerdata.customerCarTypeRequirement}\nAdditional Service: {customerdata.customerAdditionalService}\nTotal: ${customerdata.customerTotal}");
+                    cnt++;
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong! Exit the system and come back\n");
+                    break;
+                }
+
+            }
+        }
+    }
+}
+
+public void reservationss()
+
+{
+    Console.WriteLine("Enter a phone number in the format 123-456-7890:");
+    string phoneNumber = Console.ReadLine() ?? string.Empty;
+
+    // STR
+    if (phoneNumber.Length == 12 && phoneNumber[3] == '-' && phoneNumber[7] == '-')
+    {
+        string formattedNumber = phoneNumber.Replace("-", "");
+
+        Console.WriteLine("Formatted phone number: " + formattedNumber);
+    }
+    else
+    {
+        Console.WriteLine("Invalid phone number format. Please use the format 123-456-7890.");
+    }
+    // CHAR
+    if (phoneNumber.Length == 12)
+    {
+        char[] charArray = phoneNumber.ToCharArray();
+
+        if (charArray[3] == '-' && charArray[7] == '-')
+        {
+            // Remove hyphens
+            charArray[3] = charArray[7] = ' ';
+            string formattedNumber = new string(charArray).Replace(" ", "");
+
+            Console.WriteLine("Formatted phone number: " + formattedNumber);
+        }
+        else
+        {
+            Console.WriteLine("Invalid phone number format. Please use the format 123-456-7890.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid phone number length. Please use the format 123-456-7890.");
+    }
+    
+}
---
+++ midterm/Program.cs
@@ -0,0 +1,185 @@
+﻿// See https://aka.m    s/new-console-template for more information
+namespace midterm;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Displaying Book Store Details
+        Book b = new Book();
+        b.DisplayBookStoreDetails();
+
+        bool looper = true;
+
+        // Item List
+        Item itemdata = new Item();
+
+        // Hardcore Data
+        itemdata.AddItem("Republic of US", 3, 14.32);
+        itemdata.AddItem("Anna Frank", 10, 14.99);
+        itemdata.AddItem("King of Charles", 7, 7.32);
+
+        while (looper)
+        {
+
+            Console.WriteLine("Choose an option below:\n1.Add a new Item to the inventory\n2.Display all books\n3.Delete an individual item from the inventory\n4.Clear Inventory\n5.Exit the program.");
+            var path = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            // when 1 entered
+            if (path == 1)
+            {
+                // Exception Handling
+                try
+                {
+                    Console.WriteLine("Enter Book Name");
+                    string BookNameInput = Console.ReadLine() ?? string.Empty;
+
+                    Console.WriteLine("Enter Quantity in Stock:");
+                    int BookQtyInStockInput = int.Parse(Console.ReadLine() ?? string.Empty);
+
+                    Console.WriteLine("Enter Price per unit");
+                    double BookPricePerUnitInput = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                    itemdata.AddItem(BookNameInput, BookQtyInStockInput, BookPricePerUnitInput);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            }
+            else if (path == 2)
+            {
+                // display
+                itemdata.DisplayAllInventory();
+            }
+            else if (path == 3)
+            {
+                // Remove Specific Item
+                itemdata.DisplayAllInventory();
+                Console.WriteLine("\nWhich Item do you want to remove?\nWe suggest to refer Item Number by displaying all the items first");
+                int RmAt = int.Parse(Console.ReadLine() ?? string.Empty);
+                itemdata.ClearIndividualItemFromInventory(RmAt);
+                Console.Write("Item Removed Successfully!\n");
+            }
+            else if (path == 4)
+            {
+                // Clear All
+                itemdata.ClearAllInventory();
+                Console.Write("Whole Inventory Cleared Successfully!\n");
+            }
+            else if (path == 5)
+            {
+                // Exit
+                looper = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+
+
+    class Book
+    {
+        public void DisplayBookStoreDetails()
+        {
+            List<string> MyStore = new List<string>();
+
+            // String Concatenation for first and last name
+            string FirstName = "Yash ";
+            string LastName = "Mistry";
+            string Fullname = FirstName + LastName;
+            MyStore.Add("MyBookStore"); MyStore.Add(Fullname); MyStore.Add("SUNVIEW ST, WATERLOO, ON");
+            Console.WriteLine($"Store Name: {MyStore.ElementAt(0)}\nOwner: {MyStore.ElementAt(1)}\nStore Address: {MyStore.ElementAt(2)}\n");
+        }
+    }
+
+
+    // Class for inheritance Usage
+    public class ItemName
+    {
+        public string Name { get; set; }
+
+        public ItemName(string name)
+        {
+            Name = name;
+        }
+    }
+
+    // Inheritance Usage
+    public class ItemDetails : ItemName
+    {
+        // public string BookName {get; set;}
+        public int BookQuantity { get; set; }
+
+        public double BookPrice { get; set; }
+
+        public ItemDetails(string name, int qty, double price) : base(name)
+        {
+            // BookName = name;
+            BookQuantity = qty;
+            BookPrice = price;
+        }
+    }
+
+    // Interface 
+    public interface Itemable
+    {
+        void ClearAllInventory();
+        void ClearIndividualItemFromInventory(int RmAt);
+
+        void DisplayAllInventory();
+    }
+
+    // Implementing Interface
+    public class Item : Itemable
+    {
+        public List<ItemDetails> InvetoryItem = new List<ItemDetails>();
+
+        public void AddItem(string name, int BookQtyInStockInput, double BookPricePerUnitInput)
+        {
+            InvetoryItem.Add(new ItemDetails(name, BookQtyInStockInput, BookPricePerUnitInput));
+        }
+
+        public void ClearAllInventory()
+        {
+            InvetoryItem.Clear();
+            Console.WriteLine("Whole Inventory is cleared!\n");
+        }
+
+        public void DisplayAllInventory()
+        {
+            int cnt = 0;
+            double QuantityTotal = 0;
+            double PricePerUnitTotal = 0;
+            foreach (ItemDetails i in InvetoryItem)
+            {
+                try
+                {
+                    Console.WriteLine($"\nItem Number: {cnt} \nBook Name : {i.Name}\nBook in Stock: {i.BookQuantity}\nBook Price per unit: ${i.BookPrice}");
+                    cnt++;
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong! Exit the system and come back\n");
+                    break;
+                }
+
+            }
+            foreach (ItemDetails itotal in InvetoryItem)
+            {
+                QuantityTotal += itotal.BookQuantity;
+                PricePerUnitTotal += itotal.BookPrice;
+            }
+            Console.WriteLine($"\nTotal Value of the Entire Inventory: ${QuantityTotal * PricePerUnitTotal}\n");
+        }
+        public void ClearIndividualItemFromInventory(int RmAt)
+        {
+            InvetoryItem.RemoveAt(RmAt);
+        }
+    }
+
+}
