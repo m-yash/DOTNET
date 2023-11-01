de68605929a1ca5927a4375b9b72112a2dcdca6d
Yash Mistry <61091300+m-yash@users.noreply.github.com>
2023-11-01 17:21:03 -0400

    update

---
+++ WEEK8/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEEK8.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public string name {get; set;}
+    public string x {get; set;}
+
+    public bool LoginCheck {get; set;}
+
+    public List<string> weeks = new List<string>();
+    
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        
+        Console.WriteLine("true or false?");
+        x = Console.ReadLine() ?? string.Empty;
+        LoginCheck = bool.Parse(x);
+        // name = "yy" ?? string.Empty;
+        // Console.WriteLine(name);
+
+        weeks.Add("Monday");
+        weeks.Add("Tuesday");
+        weeks.Add("Wednesday");
+        weeks.Add("Thursday");
+        weeks.Add("Friday");
+        weeks.Add("Saturday");
+        weeks.Add("Sunday");
+    }
+}
